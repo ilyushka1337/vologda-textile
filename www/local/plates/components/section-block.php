8b3b30a593996ee1c6097e1da8295cc9277a3d7d
ilyushka <i.kuzmin@place-start.ru>
2024-12-23 13:58:15 +0300

    сделан необязательным $logoSrc в section-block.php

--- www/local/plates/components/section-block.php
+++ www/local/plates/components/section-block.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @var League\Plates\Template\Template $this
- * @var string $logoSrc
+ * @var string|null $logoSrc
  * @var string $imgSrc
  * @var string $overlayLogoSrc
  * @var string $link
@@ -9,7 +9,9 @@
 ?>
 
 <div class="section-block">
-    <img src="<?= $this->e($logoSrc) ?>" loading="lazy" class="section-block__logo">
+    <?php if ($logoSrc): ?>
+        <img src="<?= $this->e($logoSrc) ?>" loading="lazy" class="section-block__logo">
+    <?php endif ?>
     <img src="<?= $this->e($imgSrc) ?>" loading="lazy" class="section-block__img">
     <a href="<?= $this->e($link) ?>" class="section-block__overlay">
         <img src="<?= $this->e($overlayLogoSrc) ?>" loading="lazy" class="section-block__overlay-logo">
