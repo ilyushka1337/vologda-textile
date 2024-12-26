ea00c827124873accd4652f713164ab189dff531
ilyushka <i.kuzmin@place-start.ru>
2024-12-26 10:39:47 +0300

    главная страница теперь редактируется

--- www/index.php
+++ www/index.php
@@ -1,34 +1,39 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("1С-Битрикс: Управление сайтом");
-
-$APPLICATION->IncludeComponent('placestart:main.screen', '', [
+?>
+<? $APPLICATION->IncludeComponent('placestart:main.screen', '', [
     "CACHE_TIME" => "3600000",
     "CACHE_TYPE" => "A",
     "IS_PAGE_BLOCK" => "Y"
 ]);
-$APPLICATION->IncludeComponent('placestart:main.directions', '', [
+?>
+<? $APPLICATION->IncludeComponent('placestart:main.directions', '', [
     "CACHE_TIME" => "3600000",
     "CACHE_TYPE" => "A",
     "IS_PAGE_BLOCK" => "Y"
 ]);
-$APPLICATION->IncludeComponent('placestart:votex.sections', '', [
+?>
+<? $APPLICATION->IncludeComponent('placestart:votex.sections', '', [
     "CACHE_TIME" => "3600000",
     "CACHE_TYPE" => "A",
     "IS_PAGE_BLOCK" => "N"
 ]);
-$APPLICATION->IncludeComponent('placestart:new.section', '', [
+?>
+<? $APPLICATION->IncludeComponent('placestart:new.section', '', [
     "CACHE_TIME" => "3600000",
     "CACHE_TYPE" => "N",
     "IS_PAGE_BLOCK" => "Y",
     "TITLE" => "Новинки"
 ]);
-$APPLICATION->IncludeComponent('placestart:reviews.section', '', [
+?>
+<? $APPLICATION->IncludeComponent('placestart:reviews.section', '', [
     "CACHE_TIME" => "3600000",
     "CACHE_TYPE" => "A",
     "IS_PAGE_BLOCK" => "Y"
 ]);
-$APPLICATION->IncludeComponent(
+?>
+<? $APPLICATION->IncludeComponent(
     "placestart:about.section",
     ".default",
     array(
@@ -42,32 +47,38 @@ $APPLICATION->IncludeComponent(
     ),
     false
 );
-$APPLICATION->IncludeComponent('placestart:fabrics.section', '', [
+?>
+<? $APPLICATION->IncludeComponent('placestart:fabrics.section', '', [
     "CACHE_TIME" => "3600000",
     "CACHE_TYPE" => "A",
     "IS_PAGE_BLOCK" => "Y"
 ]);
-$APPLICATION->IncludeComponent('placestart:advantages.section', '', [
+?>
+<? $APPLICATION->IncludeComponent('placestart:advantages.section', '', [
     "CACHE_TIME" => "3600000",
     "CACHE_TYPE" => "A",
     "IS_PAGE_BLOCK" => "Y"
 ]);
-$APPLICATION->IncludeComponent('placestart:news.section', '', [
+?>
+<? $APPLICATION->IncludeComponent('placestart:news.section', '', [
     "CACHE_TIME" => "3600000",
     "CACHE_TYPE" => "A",
     "IS_PAGE_BLOCK" => "Y"
 ]);
-$APPLICATION->IncludeComponent('placestart:seo.section', '', [
+?>
+<? $APPLICATION->IncludeComponent('placestart:seo.section', '', [
     "CACHE_TIME" => "3600000",
     "CACHE_TYPE" => "A",
     "IS_PAGE_BLOCK" => "Y",
     "ELEMENT_CODE" => "seo-zagolovok-dlya-primera-v-dve-strochki",
     "IMG_POSITION" => "right"
 ]);
-$APPLICATION->IncludeComponent('placestart:partners.section', '', [
+?>
+<? $APPLICATION->IncludeComponent('placestart:partners.section', '', [
     "CACHE_TIME" => "3600000",
     "CACHE_TYPE" => "A",
     "IS_PAGE_BLOCK" => "Y"
 ]);
+?>
 
-require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php");
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
