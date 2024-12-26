004ca39a45fff24a29fb07210249bcf60e7909f2
ilyushka <i.kuzmin@place-start.ru>
2024-12-26 09:58:11 +0300

    обновление галереи

--- www/local/templates/dev-tpl/components/sprint.editor/blocks/custom/my_gallery.php
+++ www/local/templates/dev-tpl/components/sprint.editor/blocks/custom/my_gallery.php
@@ -4,42 +4,67 @@
  * @var $this  SprintEditorBlocksComponent
  */
 
-if ($block['perRow'] == 1) {
-    $images = Sprint\Editor\Blocks\Gallery::getImages(
-        $block,
-        [
-            'width' => 1260,
-            'height' => 1260,
-            'exact' => 0,
-        ]
-    );
-} else {
-    $images = Sprint\Editor\Blocks\Gallery::getImages(
-        $block,
-        [
-            'width' => 810,
-            'height' => 500,
-            'exact' => 0,
-        ]
-    );
+switch ($block['perRow']) {
+    case 1:
+        $width = 1260;
+        break;
+
+    case 2:
+        $width = 615;
+        break;
+
+    case 3:
+        $width = 400;
+        break;
+
+    case 4:
+        $width = 292;
+        break;
 }
+
+switch ($block['aspectRatio']) {
+    case '3-to-2':
+        $height = $width * 0.66;
+        break;
+
+    case '4-to-3':
+        $height = $width * 0.75;
+        break;
+
+    case '3-to-4':
+        $height = $width * 1.33;
+        break;
+
+    case 'square':
+        $height = $width;
+        break;
+}
+
+$images = Sprint\Editor\Blocks\Gallery::getImages(
+    $block,
+    [
+        'width' => ceil($width),
+        'height' => ceil($height),
+        'exact' => 1,
+    ],
+    [
+        'width' => 2000,
+        'height' => 2000,
+        'exact' => 0,
+    ]
+);
 ?>
 
 <?php if (!empty($images)): ?>
     <div class="container container-sm content-block">
         <div class="content-gallery content-gallery--cols-<?= $block['perRow'] ?>" x-data="FancyboxGallery">
             <?php foreach ($images as $image): ?>
-                <div class="content-gallery-item">
-                    <a data-fancybox='gallery' href="<?= $image['ORIGIN_SRC'] ?>" class="content-gallery-item__wrap">
-                        <img alt="<?= $image['DESCRIPTION'] ?>" src="<?= $image['SRC'] ?>" class="content-gallery-item__img">
-                        <span class="content-gallery-item__overlay">
-                            <div class="content-gallery-item__zoom icon zoom-icon"></div>
-                        </span>
-                    </a>
-                    <?php if ($image['DESCRIPTION']): ?>
-                        <p class="content-gallery-item__caption"><?= $image['DESCRIPTION'] ?></p>
-                    <?php endif ?>
-                </div>
+                <?= tpl('ui/gallery-item', [
+                    'detailUrl' => $image['DETAIL_SRC'],
+                    'previewUrl' => $image['SRC'],
+                    'caption' => $image['DESCRIPTION'],
+                    'aspectRatio' => $block['aspectRatio']
+                ]) ?>
             <?php endforeach ?>
         </div>
     </div>
