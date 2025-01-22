d58517d3a07a8f23d86340128af567e21975a11c
ilyushka <i.kuzmin@place-start.ru>
2025-01-22 14:24:13 +0300

    исправление заголовка страницы в родительских разделах каталога

--- www/local/templates/dev-tpl/components/bitrix/catalog/business-catalog/component_epilog.php
+++ www/local/templates/dev-tpl/components/bitrix/catalog/business-catalog/component_epilog.php
@@ -1,7 +1,7 @@
 <?php
 if ($arResult['IS_PARENT_SECTION']) {
     if (isset($arResult['SEO_PROPS']['SECTION_META_TITLE'])) {
-        $APPLICATION->SetTitle($arResult['SEO_PROPS']['SECTION_META_TITLE']);
+        $APPLICATION->SetPageProperty('title', $arResult['SEO_PROPS']['SECTION_META_TITLE']);
     }
 
     if (isset($arResult['SEO_PROPS']['SECTION_META_DESCRIPTION'])) {
